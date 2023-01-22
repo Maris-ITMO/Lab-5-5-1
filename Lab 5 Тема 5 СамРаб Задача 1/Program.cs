87bd461f1cdac956812a2046466e01a20b3e2594
Maris Shapiro <19maris72@gmail.com>
2023-01-22 14:24:39 +0300

    Добавьте файлы проекта.

---
+++ Lab 5 Тема 5 СамРаб Задача 1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Lab_5_Тема_5_СамРаб_Задача_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[7];
+            Console.WriteLine("Введите число");
+            int s = 0;
+            int k = 0;
+            for (int i = 0; i < 7; i++)
+            {
+                array[i] = Convert.ToInt32(Console.ReadLine());
+                if (array[i] % 2 == 0)
+                {
+                    s += array[i];
+                    k++;
+                }
+            }
+            Console.WriteLine("Среднее арифметическое равно");
+            Console.WriteLine(((double)s) / k);
+            Console.ReadKey();
+        }
+    }
+}
